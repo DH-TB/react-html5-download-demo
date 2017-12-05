16b48d474b4ab48833086b2d0e082f68a7079a87
DH-TB <929659475@qq.com>
2017-12-05 10:40:30 +0800

    finish download

---
+++ README.md
@@ -0,0 +1,1 @@
+####  HTML5 <a> download in react
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    downloadFile(fileName, content){
+        const a = this.refs.a;
+        a.download = fileName;
+        a.href = "data:text/plain," + content;
+    }
+    render() {
+
+        return (
+            <div className="App">
+                {/*服务端下载*/}
+                <a href="https://github.com/DH-TB" download="正确答案">下载</a>
+
+                {/*客户端下载*/}
+                <a ref={'a'} onClick={this.downloadFile.bind(this,'正确答案','这是文本内容')}>下载</a>
+            </div>
+        );
+    }
+}
+
+export default App;
